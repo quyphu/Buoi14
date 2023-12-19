b8ddd5296f4984bb9cd3a86f93da8ed94f51f1af
Nguyen Ngoc Phu Quy <quyphu059a@gmail.com>
2023-12-19 10:50:15 +0700

    Bai 14

---
+++ NguyenNgocPhuQuy-2033210170/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace NguyenNgocPhuQuy_2033210170
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTenMay = new System.Windows.Forms.TextBox();
+            this.txtCSDL = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnKetNoi = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên Máy :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên CSDL :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "User :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(79, 226);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Password";
+            // 
+            // txtTenMay
+            // 
+            this.txtTenMay.Location = new System.Drawing.Point(150, 77);
+            this.txtTenMay.Name = "txtTenMay";
+            this.txtTenMay.Size = new System.Drawing.Size(237, 20);
+            this.txtTenMay.TabIndex = 4;
+            this.txtTenMay.Text = "A209PC04";
+            // 
+            // txtCSDL
+            // 
+            this.txtCSDL.Location = new System.Drawing.Point(150, 126);
+            this.txtCSDL.Name = "txtCSDL";
+            this.txtCSDL.Size = new System.Drawing.Size(237, 20);
+            this.txtCSDL.TabIndex = 5;
+            this.txtCSDL.Text = "TTGV";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(150, 172);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(237, 20);
+            this.txtUser.TabIndex = 6;
+            this.txtUser.Text = "sa";
+            this.txtUser.TextChanged += new System.EventHandler(this.txtUser_TextChanged);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(150, 223);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(237, 20);
+            this.txtPassword.TabIndex = 7;
+            this.txtPassword.Text = "123";
+            // 
+            // btnKetNoi
+            // 
+            this.btnKetNoi.Location = new System.Drawing.Point(204, 270);
+            this.btnKetNoi.Name = "btnKetNoi";
+            this.btnKetNoi.Size = new System.Drawing.Size(75, 23);
+            this.btnKetNoi.TabIndex = 8;
+            this.btnKetNoi.Text = "Kết Nối";
+            this.btnKetNoi.UseVisualStyleBackColor = true;
+            this.btnKetNoi.Click += new System.EventHandler(this.btnKetNoi_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(312, 270);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 9;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 498);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnKetNoi);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.txtCSDL);
+            this.Controls.Add(this.txtTenMay);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "KẾT NỐI HỆ THỐNG...";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTenMay;
+        private System.Windows.Forms.TextBox txtCSDL;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnKetNoi;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
+
---
+++ NguyenNgocPhuQuy-2033210170/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace NguyenNgocPhuQuy_2033210170
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlDataAdapter TT_GV;
+        DataSet TT_CSGV;
+        DataColumn[] key = new DataColumn[1];
+        SqlCommand cmd = new SqlCommand();
+        string strsql;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        void Databingding(DataTable pDT)
+        {
+            txtTenMay.DataBindings.Clear();
+            txtCSDL.DataBindings.Clear();
+            txtUser.DataBindings.Clear();
+            txtPassword.DataBindings.Clear();
+
+            txtTenMay.DataBindings.Add("Text", pDT, "TenMay");
+            txtCSDL.DataBindings.Add("Text", pDT, "CSDL");
+            txtUser.DataBindings.Add("Text", pDT, "User");
+            txtPassword.DataBindings.Add("Text", pDT, "Password");
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnKetNoi_Click(object sender, EventArgs e)
+{
+    string serverName = txtTenMay.Text.Trim();
+    string databaseName = txtCSDL.Text.Trim();
+    string username = txtUser.Text.Trim();
+    string password = txtPassword.Text;
+
+    string connectionString = "Data Source="+ serverName +";Initial Catalog="+ databaseName +";User ID="+ username +";Password= "+ password +"";
+
+    using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+    {
+        try
+        {
+            sqlConnection.Open();
+
+            if (sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                MessageBox.Show("Đăng nhập thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // Nếu đăng nhập thành công, bạn có thể chuyển đến form hoặc thực hiện các thao tác khác tại đây.
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập không thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        catch (SqlException ex)
+        {
+            MessageBox.Show("Lỗi đăng nhập: {ex.Message}", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            // Hiển thị hộp thoại xác nhận
+            DialogResult result = MessageBox.Show("Bạn có chắc chắn muốn thoát không?", "Xác nhận thoát", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            // Kiểm tra kết quả
+            if (result == DialogResult.Yes)
+            {
+                // Người dùng chọn "Yes", thoát ứng dụng
+                Application.Exit();
+            }
+        }
+
+        private void txtUser_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+    }
+}
